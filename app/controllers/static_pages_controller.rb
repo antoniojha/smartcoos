af81bb8c4ae7889e2082bf6f055d03e4d8c92b57
antoniojha <antoniojha@gmail.com>
2014-09-23 02:03:29 +0000

    first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def new
+  end
+  def create
+  end
+  
+  def index
+    
+  end
+  def edit
+  end
+  
+  def update
+  end
+  def show
+  end
+  def destroy
+  end
+end
